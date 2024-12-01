57517182a3466b370bfe411c700d586df18fa95c
_shruti_rokad_ <shruti@prolegalserve.com>
2024-12-01 15:34:15 +0530

    solved the eslint error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 import Image from "next/image";
+import logo from "../app/icon.svg";
 
 export default function Home() {
   return (
     <div>
-      <div className="text-[30px] flex items-center justify-center h-[100vh] font-[butler] font-extrabold">
-        <Image
-          src={require("../app/icon.svg")}
-          alt="logo"
-          className="mr-[20px]"
-          width={50}
-          height={50}
-        />
-        Shruti&rsquo;s PortFolio
+      <div className="text-[30px] flex items-center justify-center gap-5 h-[100vh] font-[butler] font-extrabold">
+        <Image src={logo} alt="logo" width={50} height={50} />
+        <p>Shruti&rsquo;s PortFolio</p>
       </div>
     </div>
   );
