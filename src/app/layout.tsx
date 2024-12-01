de71e548b89308f3af3cbcdac3d5aeb9ba61f880
_shruti_rokad_ <shruti@prolegalserve.com>
2024-12-01 15:27:46 +0530

    completed the font-family setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,139 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+const butler = localFont({
+  src: [
+    {
+      path: "./fonts/Butler_Regular.woff",
+      weight: "400",
+    },
+    {
+      path: "./fonts/Butler_Medium.woff",
+      weight: "500",
+    },
+    {
+      path: "./fonts/Butler_Light.woff",
+      weight: "300",
+    },
+    {
+      path: "./fonts/Butler_Bold.woff",
+      weight: "700",
+    },
+    {
+      path: "./fonts/Butler_ExtraBold.woff",
+      weight: "800",
+    },
+  ],
+  variable: "--font-butler",
+  weight: "100 900",
+});
+
+const saira = localFont({
+  src: [
+    {
+      path: "./fonts/Saira-Thin.woff",
+      weight: "100",
+    },
+    {
+      path: "./fonts/Saira-ExtraLight.woff",
+      weight: "200",
+    },
+    {
+      path: "./fonts/Saira-Light.woff",
+      weight: "300",
+    },
+    {
+      path: "./fonts/Saira-Regular.woff",
+      weight: "400",
+    },
+    {
+      path: "./fonts/Saira-Medium.woff",
+      weight: "500",
+    },
+    {
+      path: "./fonts/Saira-SemiBold.woff",
+      weight: "600",
+    },
+    {
+      path: "./fonts/Saira-Bold.woff",
+      weight: "700",
+    },
+    {
+      path: "./fonts/Saira-ExtraBold.woff",
+      weight: "800",
+    },
+    {
+      path: "./fonts/Saira-Black.woff",
+      weight: "900",
+    },
+  ],
+  variable: "--font-saira",
+  weight: "100 900",
+});
+const roboto = localFont({
+  src: [
+    {
+      path: "./fonts/Roboto-Thin.woff",
+      weight: "100",
+    },
+    {
+      path: "./fonts/Roboto-Light.woff",
+      weight: "300",
+    },
+    {
+      path: "./fonts/Roboto-Regular.woff",
+      weight: "400",
+    },
+    {
+      path: "./fonts/Roboto-Medium.woff",
+      weight: "500",
+    },
+    {
+      path: "./fonts/Roboto-Bold.woff",
+      weight: "700",
+    },
+    {
+      path: "./fonts/Roboto-Black.woff",
+      weight: "900",
+    },
+  ],
+  variable: "--font-roboto",
+  weight: "100 900",
+});
+const cairo = localFont({
+  src: [
+    {
+      path: "./fonts/Cairo-Light.woff",
+      weight: "300",
+    },
+    {
+      path: "./fonts/Cairo-Regular.woff",
+      weight: "400",
+    },
+    {
+      path: "./fonts/Cairo-Medium.woff",
+      weight: "500",
+    },
+    {
+      path: "./fonts/Cairo-SemiBold.woff",
+      weight: "600",
+    },
+    {
+      path: "./fonts/Cairo-Bold.woff",
+      weight: "700",
+    },
+    {
+      path: "./fonts/Cairo-ExtraBold.woff",
+      weight: "800",
+    },
+    {
+      path: "./fonts/Cairo-Black.woff",
+      weight: "900",
+    },
+  ],
+  variable: "--font-cairo",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
   title: "Shruti's PortFolio",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="text-[30px] flex items-center justify-center h-[100vh] font-extrabold">
-      Shruti&rsquo;s PortFolio
+    <div>
+      <div className="text-[30px] flex items-center justify-center h-[100vh] font-[butler] font-extrabold">
+        <Image
+          src={require("../app/icon.svg")}
+          alt="logo"
+          className="mr-[20px]"
+          width={50}
+          height={50}
+        />
+        Shruti&rsquo;s PortFolio
+      </div>
     </div>
   );
 }
