b07820adbfea193ac7658c733e0bd7842cd051e4
_shruti_rokad_ <shruti@prolegalserve.com>
2024-12-22 16:25:18 +0530

    added the infinity skill slider.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import logo from "../app/icon.svg";
 import Navbar from "@/components/Navbar/page";
 import HomePage from "@/components/Home/page";
+import SkillAnimation from "@/components/Home/SkillAnimation";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <HomePage />
+      <SkillAnimation />
     </>
   );
 }
