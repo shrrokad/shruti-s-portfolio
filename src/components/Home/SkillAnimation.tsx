fd509c4deb3cdc7229ea7a4d8365f6a5f2c0f386
_shruti_rokad_ <rokadshruti@gmail.com>
2024-12-29 14:35:29 +0530

    converted the px to rem

--- src/components/Home/SkillAnimation.tsx
+++ src/components/Home/SkillAnimation.tsx
@@ -1,32 +1,20 @@
 import Image from "next/image";
 import React from "react";
 import "../../styles/skillanimation.css";
-
-const skills = [
-  "React JS",
-  "Next JS",
-  "AWS",
-  "React Native",
-  "Javascript",
-  "Typescript",
-  "HTML",
-  "CSS",
-  "Bootstrap",
-  "Redux",
-];
+import { skills } from "@/utils/constants";
 
 const SkillAnimation = () => {
   return (
-    <div className="h-[230px] overflow-hidden">
+    <div className="h-[29rem] overflow-hidden">
       <div
         className="mt-[8.8rem] w-full overflow-hidden whitespace-nowrap border-y border-dashed border-y-[#9682DD]"
         style={{ transform: "rotate(356deg)" }}
       >
-        <div className="scroll-animation w-[256rem] h-[12rem] py-[25px]  inline-block items-center gap-[4.6rem]">
+        <div className="scroll-animation w-[256rem] h-[12rem] py-[3.5rem]  inline-block items-center gap-[4.6rem]">
           {/* Repeating items */}
           {skills.map((skill, index) => (
-            <div key={index} className="inline-block pr-[30px]">
-              <div className="flex items-center justify-center gap-[20px]">
+            <div key={index} className="inline-block pr-[3rem]">
+              <div className="flex items-center justify-center gap-[2rem]">
                 <p className="font-[saira] text-[3.5rem]">{skill}</p>
                 <Image
                   src={require("/assets/images/icon/Star.svg")}
@@ -39,8 +27,8 @@ const SkillAnimation = () => {
             </div>
           ))}
           {skills.map((skill, index) => (
-            <div key={index} className="inline-block pr-[30px]">
-              <div className="flex items-center justify-center gap-[20px]">
+            <div key={index} className="inline-block pr-[3rem]">
+              <div className="flex items-center justify-center gap-[2rem]">
                 <p className="font-[saira] text-[3.5rem]">{skill}</p>
                 <Image
                   src={require("/assets/images/icon/Star.svg")}
