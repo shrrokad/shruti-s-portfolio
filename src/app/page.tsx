e191b9e9d56d5f20d00b7cee4179155ce4b45a4d
_shruti_rokad_ <rokadshruti@gmail.com>
2025-02-02 22:06:20 +0530

    added the Education section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar/page";
 import HomePage from "@/components/Home/page";
 import SkillAnimation from "@/components/Home/SkillAnimation";
 import MyWork from "@/components/MyWork/page";
+import EducationPage from "@/components/eduction/page";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <HomePage />
       <SkillAnimation />
       <MyWork />
+      <EducationPage />
     </>
   );
 }
---
+++ src/components/eduction/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Eduction from "./Eduction";
+
+export default function EducationPage() {
+  return <Eduction />;
+}
