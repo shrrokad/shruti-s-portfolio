0cac6ebfe562ce160c9c0eafebae63ad8cabcc48
_shruti_rokad_ <rokadshruti@gmail.com>
2025-02-08 18:11:31 +0530

    Solved the css issue in common components.

--- src/common/HeadingText.tsx
+++ src/common/HeadingText.tsx
@@ -5,15 +5,34 @@ interface HeadingTextProps {
 }
 const HeadingText = ({ label }: HeadingTextProps) => {
   return (
-    <div className="relative min-w-[35.6rem] max-w-[41.6rem] ">
-      <p className="text-[8rem] font-[cairo] font-extrabold text-[#9682DD]">
+    <div
+      style={{ position: "relative", width: "41.6rem" }}
+      //maxWidth: "41.6rem", minWidth: "35.6rem"
+    >
+      <p
+        // className="font-[cairo] font-extrabold text-[#9682DD]"
+        style={{
+          fontFamily: "Cairo, sans-serif",
+          fontWeight: 800, // equivalent to 'font-extrabold'
+          color: "#9682DD",
+          fontSize: "8rem",
+        }}
+      >
         {label}{" "}
       </p>
       <p
-        className="text-[8rem] font-[cairo] font-extrabold text-[#161519] absolute top-[0.3rem] left-[0.3rem] -z-[1]"
+        // className="font-[cairo] font-extrabold text-[#161519] -z-[1]"
         style={{
+          fontFamily: "Cairo, sans-serif",
+          fontWeight: 800,
+          color: "#161519",
+          position: "absolute",
+          top: "0.3rem",
+          left: "0.3rem",
+          fontSize: "8rem",
           textShadow:
             "0.1rem 0.1rem #fff, 0rem 0rem #fff, 0rem 0rem #fff, 0rem 0rem #fff",
+          zIndex: -1, // equivalent to '-z-[1]'
         }}
       >
         {label}{" "}
